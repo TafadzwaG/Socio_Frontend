7c7135bf4bacacdb143549531db381905abf2292
TafadzwaG <tafadzwagashirah@gmail.com>
2022-12-09 12:03:36 +0200

    social front-end

---
+++ src/scenes/loginPage/index.jsx
@@ -0,0 +1,37 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
+
+const LoginPage = () => {
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  return (
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+       
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          SascoChat
+        </Typography>
+      </Box>
+
+      <Box
+        width={isNonMobileScreens ? "30%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to SascoChat, the Social Media for Geeks!
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
+  );
+};
+
+export default LoginPage;
